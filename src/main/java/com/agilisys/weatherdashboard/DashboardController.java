54d7a72d8c0b9b2afc1aa602ce820bdd61359850
Julian.Mojico <jmojico@gmail.com>
2017-08-23 19:15:43 -0300

    +HTTPRequest -working in controller, update method

--- src/main/java/com/agilisys/weatherdashboard/DashboardController.java
+++ src/main/java/com/agilisys/weatherdashboard/DashboardController.java
@@ -9,17 +9,17 @@ package com.agilisys.weatherdashboard;
  *
  * @author Julian
  */
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,17 +28,37 @@ public class DashboardController {
     @Autowired
     private DashboardRepo repo;
     
+    
+    //get Dashboard by Id (name)
     @RequestMapping(method = RequestMethod.GET, value = "/dashboards/{id}")
     
-    public @ResponseBody Dashboard dashboard(@Validated @PathVariable("id") String id) {
+    public @ResponseBody Dashboard getDashboard(@Validated @PathVariable("id") String id) {
         return repo.findOne(id);
     }
     
+    //add new dashboard
+    @RequestMapping(method = RequestMethod.POST, value = "/dashboards/update",produces = "application/json",consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    public void postDashboard(@RequestBody Dashboard db) {
+        
+        //repo.insert(db);
+        repo.save(db);
+         
+    }
+    
+    //get full list of  Dashboards
     @RequestMapping(method = RequestMethod.GET, value = "/dashboards")
     
-    public  @ResponseBody List<Dashboard> dashboard() {
+    public  @ResponseBody Object[] getAllDashboards() {
        
-        return repo.findAll();
+        /*
+        StringBuilder sb = new StringBuilder();
+        ArrayList dashboards = (ArrayList) repo.findAll();
+        dashboards.forEach((dashboard)->sb.append(dashboard.toString()));
+        return sb.toString(); */
+        
+        return repo.findAll().toArray();
 // return new ResponseEntity<List<JSONObject>>(entities,HttpStatus.OK);
                 //repo.findAll();
     }
