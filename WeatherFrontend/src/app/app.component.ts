31cee263dbbbe5a9725f650aaef393efcb58f955
Julian.Mojico <jmojico@gmail.com>
2017-09-19 19:23:26 -0300

    Front end layout initial

--- WeatherFrontend/src/app/app.component.ts
+++ WeatherFrontend/src/app/app.component.ts
@@ -13,75 +13,41 @@ export class AppComponent {
 
 
   //generate input simulation
-
-  dash1: Dashboard = {
-
-    id: "Fulano",
-    locations: [ {woeid:123,locationName:"invalidLocation"},{woeid:2487889,"locationName" : "San Diego, CA"}   ]
-    }
+ 
   
-
   initialDashboard = new Dashboard();
-  private dashboards = ['Fulano','Mengano'];
-
-  //generate input simulation
-  
-  private subscription : any;
   title = 'Weather Dashboard';
   selectedDash :  Dashboard;
   dashboardList : Dashboard[];
-  se
+
+
+  constructor() {
+  
+  this.dashboardList = [
+
+      //generate input simulation
+    {
+
+    id: "Fulano",
+    locations: [ {woeid:123,locationName:"invalidLocation"},{woeid:2487889,"locationName" : "San Diego, CA"}   ]
+    },
+
+    {
+
+    id: "Mengano",
+    locations: [ {woeid:64189,locationName:"	Ciudad de la Habana, Cuba"},{woeid:2338936,"locationName" : "Istanbul, Buyukcekmece"}   ]
+  }
+  ]
+  
+}
+
+  getId (dash:Dashboard): string {
+    return dash.id;
+  }
  
    onSelect (dash:Dashboard): void {
     this.selectedDash = dash
    }
-
-  //response 
-    public response = (data) => {
-    console.log(data)
-  }
-
-  constructor(stomp: StompService) {
-    
-   
-  
-     //configuration 
-     stomp.configure({
-  
-       host:'http://127.0.0.1:8080/api',
-       debug:true,
-       queue:{'init':true}
-     });
-     
-     //start connection 
-     stomp.startConnect().then(() => {
-       stomp.done('init');
-       console.log('connected');
-       
-       //subscribe 
-       this.subscription = stomp.subscribe('/feed', this.response);
-       
-       //send data 
-       
-       stomp.send("/app/status", this.selectedDash );
-       
-       /*
-       //unsubscribe 
-       this.subscription.unsubscribe();
-       
-       //disconnect 
-       stomp.disconnect().then(() => {
-         console.log( 'Connection closed' )
-       })
-       */
-     });
-    
-
-
-
-}
-
-
 
 }
 
